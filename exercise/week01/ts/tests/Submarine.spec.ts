4c549de20aaacf8cd36481978eb79d90e1b91b0e
TOMYAAAAAH <tom.almarcha@protonmail.com>
2025-07-04 17:16:29 +0200

    exercice 1 done

--- exercise/week01/ts/tests/Submarine.spec.ts
+++ exercise/week01/ts/tests/Submarine.spec.ts
@@ -1,6 +1,28 @@
+
+import * as fs from 'fs';
+
+const rawFile:string = fs.readFileSync('tests/submarine.txt', 'utf-8');
+const lines:string[] = rawFile.split('\n');
+
+let horizontal:number = 0;
+let depth:number = 0;
+
+lines.forEach(line => {
+    let [command, value]:string[] = line.split(' ');
+    if (command === "forward") {
+        horizontal+=Number(value);
+    } else if(command === "up"){
+        depth-=Number(value);
+    } else if(command === "down"){
+        depth+=Number(value);
+    }
+})
+
+
+
 describe('submarine should move', function () {
     test('on given text instructions', () => {
         // TODO find the right result by implementing the Submarine logic
-        expect(43).toBe(42);
+        expect(horizontal*depth).toBe(1690020);
     })
 })
