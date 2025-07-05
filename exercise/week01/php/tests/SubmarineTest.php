7c650a1f640512ab5412283001dd8373ff27b0f7
TOMYAAAAAH <tom.almarcha@protonmail.com>
2025-07-05 21:14:30 +0200

    week 1 done in php too

--- exercise/week01/php/tests/SubmarineTest.php
+++ exercise/week01/php/tests/SubmarineTest.php
@@ -1,6 +1,28 @@
 <?php
 
+$horizontal=0;
+$depth=0;
+
+$rawFile = file_get_contents(__DIR__ . "/submarine.txt") or die("Unable to open file");
+
+$lines = explode(PHP_EOL, $rawFile);
+
+foreach($lines as $line){
+    [$command, $value] = explode(" ", $line);
+    if ($command==="forward") {
+        $horizontal+=$value;
+    } elseif ($command==="up") {
+        $depth-=$value;
+    } elseif ($command==="down") {
+        $depth+=$value;
+    }
+}
+
+echo $horizontal," x ",$depth," = ",$horizontal*$depth;
+
 test('should move on given text instructions', function () {
     // TODO: Implement the submarine logic and replace this placeholder value
-    expect(43)->toBe(42, 'it is universal answer');
+    expect($horizontal*$depth)->toBe(1690020, 'it is universal answer');
 });
+
+?>
